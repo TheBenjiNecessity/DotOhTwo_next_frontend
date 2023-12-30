f80057a0ccff109cb4a28904070ad942bcf0f7c4
TheBenjiNecessity <TheBenjiNecessity@users.noreply.github.com>
2023-12-29 21:56:12 -0500

    Move auth config to auth.ts

--- auth.ts
+++ auth.ts
@@ -1,4 +1,3 @@
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import type {
     GetServerSidePropsContext,
     NextApiRequest,
@@ -6,11 +5,36 @@ import type {
 } from "next";
 import type { NextAuthOptions } from "next-auth";
 import { getServerSession } from "next-auth";
+import GithubProvider from "next-auth/providers/github";
 
-// You'll need to import and pass this
-// to `NextAuth` in `app/api/auth/[...nextauth]/route.ts`
 export const config = {
-    providers: [], // rest of your config
+    secret: process.env.NEXTAUTH_SECRET,
+    providers: [
+        GithubProvider({
+            clientId: process.env.GITHUB_ID as string,
+            clientSecret: process.env.GITHUB_SECRET as string,
+        }),
+    ],
+    callbacks: {
+        async session({ session, token }) {
+            const newSession = session as any;
+            newSession.token = token;
+            return newSession;
+        },
+        async jwt({ account, token }) {
+            if (account) {
+                token.accessToken = account.access_token;
+            }
+            return token;
+        },
+    },
+    pages: {
+        signIn: "/auth/signin",
+        signOut: "/auth/signout",
+        error: "/auth/error", // Error code passed in query string as ?error=
+        verifyRequest: "/auth/verify-request", // (used for check email message)
+        newUser: "/auth/new-user", // New users will be directed here on first sign in (leave the property out if not of interest)
+    },
 } satisfies NextAuthOptions;
 
 // Use it in server contexts
