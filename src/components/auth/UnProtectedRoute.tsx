7ab6c79b9101474ba595cb7c48a07741efe0cfdd
TheBenjiNecessity <TheBenjiNecessity@users.noreply.github.com>
2024-01-01 20:22:56 -0500

    Add UnProtectedRoute comp

---
+++ src/components/auth/UnProtectedRoute.tsx
@@ -0,0 +1,30 @@
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+import { config } from "../../../auth";
+
+/**
+ * A route component that only shows its content if the user is signed out.
+ * Otherwise, the user is redirected.
+ * @param redirectUrl {string} the url to redirect to if the user is signed in
+ */
+export default async function UnProtectedRoute({
+    redirectToUser = true,
+    redirectUrl = "/",
+    children,
+}: {
+    redirectToUser?: boolean;
+    redirectUrl?: string;
+    children: React.ReactNode;
+}) {
+    const session = await getServerSession(config);
+
+    if (session) {
+        if (redirectToUser) {
+            return redirect(`/${session.name}`);
+        }
+
+        return redirect(redirectUrl);
+    }
+
+    return <>{children}</>;
+}
