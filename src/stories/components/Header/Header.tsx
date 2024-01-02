ec67e474e2d3d4e533306aa721489e07993de144
TheBenjiNecessity <TheBenjiNecessity@users.noreply.github.com>
2024-01-02 10:24:40 -0500

    Clean up header comp

--- src/stories/components/Header/Header.tsx
+++ src/stories/components/Header/Header.tsx
@@ -1,73 +1,70 @@
-import React from "react";
+import React, { ReactNode } from "react";
 
 import { Button } from "../Button/Button";
 import "./header.css";
 import "../../../app/globals.css";
+import { Stack } from "../Stack/Stack";
+import { Flexbox } from "../Flexbox/Flexbox";
 
 type User = {
     name: string;
 };
 
-interface HeaderProps {
+interface HeaderUserMessageProps {
     user?: User;
-    onLogin: () => void;
-    onLogout: () => void;
-    onCreateAccount: () => void;
 }
 
-export const Header = ({
-    user,
-    onLogin,
-    onLogout,
-    onCreateAccount,
-}: HeaderProps) => (
+interface HeaderProps {
+    children?: ReactNode;
+}
+
+export const HeaderBranding = () => (
+    <Stack direction="row" spacing={1} alignItems="center">
+        <svg
+            width="32"
+            height="32"
+            viewBox="0 0 32 32"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <g fill="none" fillRule="evenodd">
+                <path
+                    d="M10 0h12a10 10 0 0110 10v12a10 10 0 01-10 10H10A10 10 0 010 22V10A10 10 0 0110 0z"
+                    fill="#FFF"
+                />
+                <path
+                    d="M5.3 10.6l10.4 6v11.1l-10.4-6v-11zm11.4-6.2l9.7 5.5-9.7 5.6V4.4z"
+                    fill="#555AB9"
+                />
+                <path
+                    d="M27.2 10.6v11.2l-10.5 6V16.5l10.5-6zM15.7 4.4v11L6 10l9.7-5.5z"
+                    fill="#91BAF8"
+                />
+            </g>
+        </svg>
+        <h1 className="font-bold">Dot Oh Two</h1>
+    </Stack>
+);
+
+export const HeaderUserMessage = ({ user }: HeaderUserMessageProps) => {
+    if (!user) return null;
+
+    return (
+        <span className="text-gray-800 text-sm">
+            Welcome, <b className="text-black">{user.name}</b>!
+        </span>
+    );
+};
+
+export const Header = ({ children }: HeaderProps) => (
     <header>
-        <div className="storybook-header">
-            <div>
-                <svg
-                    width="32"
-                    height="32"
-                    viewBox="0 0 32 32"
-                    xmlns="http://www.w3.org/2000/svg"
-                >
-                    <g fill="none" fillRule="evenodd">
-                        <path
-                            d="M10 0h12a10 10 0 0110 10v12a10 10 0 01-10 10H10A10 10 0 010 22V10A10 10 0 0110 0z"
-                            fill="#FFF"
-                        />
-                        <path
-                            d="M5.3 10.6l10.4 6v11.1l-10.4-6v-11zm11.4-6.2l9.7 5.5-9.7 5.6V4.4z"
-                            fill="#555AB9"
-                        />
-                        <path
-                            d="M27.2 10.6v11.2l-10.5 6V16.5l10.5-6zM15.7 4.4v11L6 10l9.7-5.5z"
-                            fill="#91BAF8"
-                        />
-                    </g>
-                </svg>
-                <h1>Dot Oh Two</h1>
-            </div>
-            <div>
-                {user ? (
-                    <>
-                        <span className="welcome">
-                            Welcome, <b>{user.name}</b>!
-                        </span>
-                        <Button size="small" onClick={onLogout}>
-                            Log out
-                        </Button>
-                    </>
-                ) : (
-                    <>
-                        <Button size="small" onClick={onLogin}>
-                            Log in
-                        </Button>
-                        <Button primary size="small" onClick={onCreateAccount}>
-                            Sign up
-                        </Button>
-                    </>
-                )}
-            </div>
+        <div className="border-b border-black border-solid px-2 py-1">
+            <Flexbox
+                direction="row"
+                alignItems="center"
+                justifyContent="space-between"
+            >
+                {children}
+            </Flexbox>
         </div>
     </header>
 );
