09ba54f620ffc1b03b186f1aa7412420073df4fe
TheBenjiNecessity <TheBenjiNecessity@users.noreply.github.com>
2024-01-01 20:23:42 -0500

    Use unprotectedroute comp

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -1,26 +1,15 @@
 import { getProviders } from "next-auth/react";
-import { getServerSession } from "next-auth/next";
 import AuthProviderButton from "@/components/auth/AuthProviderButton";
-import { redirect } from "next/navigation";
-import { config } from "../../../../auth";
+import UnProtectedRoute from "@/components/auth/UnProtectedRoute";
 
 export default async function SignIn() {
-    const session = await getServerSession(config);
-
-    // If the user is already logged in, redirect.
-    if (session) {
-        return redirect("/");
-    }
-
     const providers = await getProviders();
 
-    console.log("providers", providers);
-
     return (
-        <>
+        <UnProtectedRoute>
             {Object.values(providers || []).map((provider) => (
                 <AuthProviderButton key={provider.id} provider={provider} />
             ))}
-        </>
+        </UnProtectedRoute>
     );
 }
