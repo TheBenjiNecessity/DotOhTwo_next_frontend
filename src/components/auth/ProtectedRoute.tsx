907a901969a0671c2c92e0628c558f4a447a8b0e
TheBenjiNecessity <TheBenjiNecessity@users.noreply.github.com>
2024-01-01 20:23:12 -0500

    Add ability to set redirect url

--- src/components/auth/ProtectedRoute.tsx
+++ src/components/auth/ProtectedRoute.tsx
@@ -3,14 +3,16 @@ import { redirect } from "next/navigation";
 import { config } from "../../../auth";
 
 export default async function ProtectedRoute({
+    redirectUrl = "/",
     children,
 }: {
+    redirectUrl?: string;
     children: React.ReactNode;
 }) {
     const session = await getServerSession(config);
 
     if (!session) {
-        return redirect("/auth/signin"); // TODO get from central source
+        return redirect(redirectUrl);
     }
 
     return <>{children}</>;
