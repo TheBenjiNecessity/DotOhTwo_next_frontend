4946e72c09292d2a9e996e97b15212f4d1090477
TheBenjiNecessity <TheBenjiNecessity@users.noreply.github.com>
2024-01-15 20:43:15 -0500

    Remove unused route

--- src/app/review/route.ts
+++
@@ -1,6 +0,0 @@
-import { type NextApiRequest, type NextApiResponse } from "next";
-import { request } from "@/services/nextRestApi.service";
-
-export async function GET(req: NextApiRequest) {
-    return request(req);
-}
