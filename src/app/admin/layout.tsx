1918ee7d3d6f7652c5b50644ccb4681eba05dbfb
TheBenjiNecessity <TheBenjiNecessity@users.noreply.github.com>
2024-01-19 22:35:22 -0500

    Fix styles

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -5,7 +5,7 @@ export default async function Page({ children }: { children: ReactNode }) {
     return (
         <ProtectedRoute roles={["ADMIN"]}>
             <main className="flex min-h-screen flex-col items-center justify-between p-24">
-                <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+                <div className="z-10 max-w-5xl w-full items-center justify-center font-mono text-sm lg:flex">
                     {children}
                 </div>
             </main>
