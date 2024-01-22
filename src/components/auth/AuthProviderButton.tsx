289aa0eada7a0f57cd6f38ddf65f377352a20954
TheBenjiNecessity <TheBenjiNecessity@users.noreply.github.com>
2024-01-22 13:59:16 -0500

    Add SocialLoginButton comp to auth button

--- src/components/auth/AuthProviderButton.tsx
+++ src/components/auth/AuthProviderButton.tsx
@@ -1,15 +1,13 @@
 "use client";
 
+import { SocialLoginButton } from "@/stories/components/SocialLoginButton/SocialLoginButton";
 import { signIn } from "next-auth/react";
 
 export default function AuthProviderButton({ provider }: any) {
     return (
-        <button onClick={() => signIn(provider.id)}>
-            Sign in with {provider.name}
-        </button>
+        <SocialLoginButton
+            providerId={provider.id}
+            onClick={() => signIn(provider.id)}
+        />
     );
 }
-
-//http://localhost:8080/login/oauth2/code/github
-
-// http://localhost:8080/login/oauth2/authorization/github
