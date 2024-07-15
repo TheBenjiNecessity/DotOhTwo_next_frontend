4c5103dcf0c6d7346a81f2a6717c0da9aee71ed9
TheBenjiNecessity <TheBenjiNecessity@users.noreply.github.com>
2024-07-15 09:10:30 -0400

    Add card sub comps to page

--- src/stories/pages/CompleteProfile/CompleteProfile.tsx
+++ src/stories/pages/CompleteProfile/CompleteProfile.tsx
@@ -5,10 +5,11 @@ import {
     FormItem,
     FormLabel,
 } from "../../components/Form/Form";
-import Card from "../../components/Card/Card";
+import Card, { CARD_WIDTH } from "../../components/Card/Card";
 import React from "react";
 import Input from "../../components/Input/Input";
 import { FieldValues, UseFormReturn } from "react-hook-form";
+import { Button } from "../../components/Button/Button";
 
 interface CompleteProfileProps {
     form: UseFormReturn<FieldValues, any, undefined>;
@@ -16,16 +17,19 @@ interface CompleteProfileProps {
 
 export const CompleteProfile = ({ form }: CompleteProfileProps) => {
     return (
-        <section>
-            <Card>
-                <Card.Header>Complete Profile</Card.Header>
+        <section className="flex justify-center items-center w-full h-full">
+            <Card width={CARD_WIDTH.MEDIUM}>
+                <Card.Header centered>Complete Profile</Card.Header>
+                <Card.Body>
                 <Form {...form}>
                     <FormField
                         control={form.control}
                         name="username"
                         render={() => (
                             <FormItem>
-                                <FormLabel htmlFor="name">User name</FormLabel>
+                                    <FormLabel htmlFor="name">
+                                        User name
+                                    </FormLabel>
                                 <FormControl>
                                     <Input placeholder="User Name" />
                                 </FormControl>
@@ -46,6 +50,15 @@ export const CompleteProfile = ({ form }: CompleteProfileProps) => {
                         )}
                     />
                 </Form>
+                </Card.Body>
+                <Card.Footer>
+                    <div className="flex justify-center items-center w-full">
+                        <div className="flex-grow"></div>
+                        <div className="flex-shrink">
+                            <Button primary>Submit</Button>
+                        </div>
+                    </div>
+                </Card.Footer>
             </Card>
         </section>
     );
