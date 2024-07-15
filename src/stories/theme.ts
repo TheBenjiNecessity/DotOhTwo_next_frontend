059ac4d18b04aae71340a5c027f92d1a9461ba85
TheBenjiNecessity <TheBenjiNecessity@users.noreply.github.com>
2024-07-15 09:08:37 -0400

    Fix theme styles

--- src/stories/theme.ts
+++ src/stories/theme.ts
@@ -1,74 +1,35 @@
-const palette = {
-    primary: {
-        main: "blue-400",
-        light: "blue-200",
-        dark: "blue-600",
-        contrastText: "white",
-    },
-    secondary: {
-        main: "gray-200",
-        light: "gray-100",
-        dark: "gray-400",
-        contrastText: "black",
-    },
-    error: {
-        main: "red-400",
-        light: "red-200",
-        dark: "red-600",
-        contrastText: "white",
-    },
-    warning: {
-        main: "orange-400",
-        light: "orange-200",
-        dark: "orange-600",
-        contrastText: "black",
-    },
-    info: {
-        main: "blue-800",
-        light: "blue-600",
-        dark: "blue-900",
-        contrastText: "white",
-    },
-    success: {
-        main: "green-400",
-        light: "green-200",
-        dark: "green-600",
-        contrastText: "white",
-    },
-};
-
 const theme = {
     button: {
         primary: {
             contained: {
-                color: `text-${palette.primary.contrastText}`,
-                backgroundColor: `bg-${palette.primary.main}`,
+                color: "text-white",
+                backgroundColor: "bg-blue-400",
                 borderColor: "transparent",
             },
             outlined: {
-                color: `text-${palette.primary.main}`,
+                color: "text-blue-400",
                 backgroundColor: "transparent",
-                borderColor: `border-${palette.primary.main}`,
+                borderColor: "border-blue-400",
             },
             text: {
-                color: `text-${palette.primary.main}`,
+                color: "text-blue-400",
                 backgroundColor: "transparent",
                 borderColor: "transparent",
             },
         },
         secondary: {
             contained: {
-                color: `text-${palette.secondary.contrastText}`,
-                backgroundColor: `bg-${palette.secondary.main}`,
+                color: "text-black",
+                backgroundColor: "bg-gray-400",
                 borderColor: "transparent",
             },
             outlined: {
-                color: `text-${palette.secondary.main}`,
+                color: "text-gray-400",
                 backgroundColor: "transparent",
-                borderColor: `border-${palette.secondary.main}`,
+                borderColor: "border-gray-400",
             },
             text: {
-                color: `text-${palette.secondary.main}`,
+                color: "text-black",
                 backgroundColor: "transparent",
                 borderColor: "transparent",
             },
