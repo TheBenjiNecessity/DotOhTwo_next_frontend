db3b62ae32dca34901a2f742d8cca17766703e02
TheBenjiNecessity <TheBenjiNecessity@users.noreply.github.com>
2024-07-29 20:00:33 -0400

    Add validation

--- src/components/pages/complete-profile/formAction.ts
+++ src/components/pages/complete-profile/formAction.ts
@@ -1,18 +1,33 @@
 "use server";
 
+import { z } from "zod";
 import { CompleteProfileFormValues } from "@/stories/pages/CompleteProfile/CompleteProfile";
+
+const schema = z.object({
+    username: z.string().min(3, { message: "Username is too short" }),
+    email: z.string().email({ message: "Invalid email" }),
+});
 
 export async function onFormAction(
     prevState: CompleteProfileFormValues,
     formData: FormData
 ) {
-    // console.log("onFormAction", prevState, formData);
-
-    // console.log("username", formData.get("username"));
-    // console.log("email", formData.get("email"));
     // Process the data
+    const validatedFields = schema.safeParse({
+        username: formData.get("username"),
+        email: formData.get("email"),
+    });
+
+    // Return early if the form data is invalid
+    if (!validatedFields.success) {
+        return {
+            errors: validatedFields.error.flatten().fieldErrors,
+        };
+    }
+
+    // TODO - send the data to the server
+
     return {
-        username: "Form data processed",
-        email: "",
+        errors: null,
     };
 }
