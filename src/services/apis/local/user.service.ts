920c046034b8153d7dff8cd743c4c6bd4b128d7b
TheBenjiNecessity <TheBenjiNecessity@users.noreply.github.com>
2024-07-29 22:53:52 -0400

    Code cleanup

--- src/services/apis/local/user.service.ts
+++ src/services/apis/local/user.service.ts
@@ -1,9 +1,7 @@
-import { User } from "@/models/user.model";
 import { get } from "@/services/apis/local/restapi.service";
 
 export const getUser = (username?: string) => {
     const urlString = username ? `/user?username=${username}` : "/user";
-    ``;
 
     return get(urlString);
 };
