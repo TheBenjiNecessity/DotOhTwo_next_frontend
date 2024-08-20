e1178c8a5f5537ce27782b7eb1d9c143b32f11fe
TheBenjiNecessity <TheBenjiNecessity@users.noreply.github.com>
2024-08-19 22:47:01 -0400

    Add more details to comp

--- src/stories/components/Review/ReviewListItem.stories.tsx
+++ src/stories/components/Review/ReviewListItem.stories.tsx
@@ -15,6 +15,25 @@ type Story = StoryObj<typeof meta>;
 
 export const ReviewOne: Story = {
     args: {
-        review: { name: "review" },
+        review: {
+            name: "Ut enim ad minim veniam",
+            description: `
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                sed do eiusmod tempor incididunt ut labore et dolore magna
+                aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                Duis aute irure dolor in reprehenderit in voluptate velit
+                esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+                occaecat cupidatat non proident, sunt in culpa qui officia
+                deserunt mollit anim id est laborum.
+            `,
+            username: "username",
+            rating: 5,
+            date: new Date(),
+            content: {
+                avatarUrl: undefined,
+                reviewableAvatarUrl: undefined,
+            },
+        },
     },
 };
--- src/stories/components/Review/ReviewListItem.tsx
+++ src/stories/components/Review/ReviewListItem.tsx
@@ -1,7 +1,21 @@
 import React from "react";
+import Stack from "../Stack/Stack";
+import { Avatar } from "../Avatar/Avatar";
+import { Typography } from "../Typography/Typography";
+import { Rating } from "../Rating/Rating";
+
+type ReviewContent = {
+    avatarUrl?: string;
+    reviewableAvatarUrl?: string;
+};
 
 type Review = {
     name: string;
+    description: string;
+    username: string;
+    rating: number;
+    date: Date;
+    content: ReviewContent;
 };
 
 interface ReviewProps {
@@ -13,4 +27,40 @@ interface ReviewProps {
  * @param param0
  * @returns
  */
-export const Review = ({ review }: ReviewProps) => <div>review</div>;
+export const Review = ({ review }: ReviewProps) => (
+    <div className="p-4">
+        <Stack direction="row" spacing={2}>
+            <Stack.Expander>
+                <Stack spacing={2}>
+                    <Stack direction="row" spacing={2}>
+                        <div>
+                            <Avatar
+                                imageUrl={review?.content?.avatarUrl}
+                                size="large"
+                            />
+                        </div>
+                        <Stack>
+                            <Typography variant="h4">
+                                {review?.username}
+                            </Typography>
+                            <Typography variant="h4">
+                                {review?.username}
+                            </Typography>
+                        </Stack>
+                    </Stack>
+                    <Rating score={review?.rating} />
+                    <Typography variant="h4">{review?.name}</Typography>
+                    <Typography variant="body1">
+                        {review?.description}
+                    </Typography>
+                </Stack>
+            </Stack.Expander>
+            <div className="min-w-[200px]">
+                <Avatar
+                    imageUrl={review?.content?.reviewableAvatarUrl}
+                    size="large"
+                />
+            </div>
+        </Stack>
+    </div>
+);
